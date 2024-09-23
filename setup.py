c6999443dc8f6632c6f5b4243949cf353b003cee
sdarjunwadkar <sdarjunwadkar@gmail.com>
2024-09-23 15:37:52 -0400

    install_requirs in setup to import necessary packages when imported

--- setup.py
+++ setup.py
@@ -25,4 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requirs = ["pymongo", "pymongo[srv]", "dnspython", "pandas", "numpy", "ensure", "pytest"]
     )
